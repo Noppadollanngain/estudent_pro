dc45b9561af9422ab97f3ae65853f6c481cc21cc
Noppadol Lanngain <noppadollanngain@gmail.com>
2019-04-04 22:30:03 +0700

    http

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,37 +12,18 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    // public function register()
-    // {
-    //     //
-    // }
+    public function register()
+    {
+        //
+    }
 
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    // public function boot()
-    // {
-    //     Schema::defaultStringLength(191);
-    // }
-
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
-        if(env('REDIRECT_HTTPS')) {
-            $url->formatScheme('https');
-        }
-    }
-
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        if(env('REDIRECT_HTTPS')) {
-            $this->app['request']->server->set('HTTPS', true);
-        }
+        Schema::defaultStringLength(191);
     }
 }
