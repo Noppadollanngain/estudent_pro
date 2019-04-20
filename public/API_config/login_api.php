b4891ac826420c017ec20b2c14a9fb3187d28be4
Noppadollanngain <42247295+Noppadollanngain@users.noreply.github.com>
2019-04-20 13:26:40 +0700

    Update login_api.php

--- public/API_config/login_api.php
+++ public/API_config/login_api.php
@@ -13,7 +13,7 @@
     $result_1 = mysqli_fetch_array(mysqli_query($con,$query_1));
 
     if( $result_1[0] == 0){
-        exit(json_encode(['state'=>0, 'msg'=> 'miss'. $result_1[0] ]));
+        exit(json_encode(['state'=>0, 'msg'=> 'miss123131313']));
     }else{
         $query_2 = "SELECT COUNT(`stdId`) FROM `students` WHERE `stdId` = '".$data['username']."' AND `peopleId` = '".$data['password']."'";
         $result_2 =  mysqli_fetch_array(mysqli_query($con,$query_2));
