0c22a15fe5898dbd4753cf667748c7b12e4d7180
Noppadol Lanngain <noppadollanngain@gmail.com>
2019-04-21 15:15:20 +0700

    adasdasd

--- public/API_config/login_api.php
+++ public/API_config/login_api.php
@@ -6,8 +6,8 @@
     file_put_contents('log/logs', $request_body.PHP_EOL, FILE_APPEND);
     $data = json_decode($request_body,true);
     
-//     $data['username'] = '59544401061-4';
-//     $data['password'] = '1509970034069';
+    $data['username'] = '59544401061-4';
+    $data['password'] = '1509970034069';
      
     $query_1 = "SELECT COUNT(`stdId`) FROM `students` WHERE `stdId` = '".$data['username']."'";
     $result_1 = mysqli_fetch_array(mysqli_query($con,$query_1));
@@ -23,6 +23,7 @@
             $data_show = "select * from documents join students on documents.student = students.id where  students.stdId = '".$data['username']."' and students.peopleId = '".$data['password'].'"';
             $reusult_test = mysqli_query($con,$data_show);
             $result_show = mysqli_fetch_assoc($reusult_test);
+            echo $data_show;
 
             if($result_show['loginstatus']){
                 exit(json_encode(['state'=>0, 'msg'=>'login already']));
