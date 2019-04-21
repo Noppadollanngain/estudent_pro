29a34c9c1d13945c2b95c5915bcda7416c88895b
Noppadol Lanngain <noppadollanngain@gmail.com>
2019-04-21 15:11:49 +0700

    test

--- public/API_config/login_api.php
+++ public/API_config/login_api.php
@@ -20,13 +20,12 @@
         if( $result_2[0]==0){
             exit(json_encode(['state'=>0, 'msg'=>'erro']));
         }else{
-            $data_show = 'SELECT students.*,documents.typestudent FROM `students` LEFT JOIN `documents` ON students.id = documents.student WHERE `stdId` = "'.$data['username'].'" AND `peopleId` = "'.$data['password'].'"';
+            $data_show = "select * from documents join students on documents.student = students.id where  students.stdId = '".$data['username']."' and students.peopleId = '".$data['password'].'"';
+            $reusult_test = mysqli_query($con,$data_show);
             $result_show = mysqli_fetch_assoc($reusult_test);
-            echo $data_show;
 
             if($result_show['loginstatus']){
                 exit(json_encode(['state'=>0, 'msg'=>'login already']));
-//                 exit(json_encode($result_show));
             }else{
                 $update_status = 'UPDATE `students` SET `loginstatus`=  1 WHERE `stdId` = "'.$data['username'].'" AND `peopleId` = "'.$data['password'].'"';
                 mysqli_query($con,$update_status);
