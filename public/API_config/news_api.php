b0a7b39a5131cf43d747de129d48e8a1a42f7ce3
Noppadol Lanngain <noppadollanngain@gmail.com>
2019-04-22 19:17:16 +0700

    [Edited] API type 4

--- public/API_config/news_api.php
+++ public/API_config/news_api.php
@@ -5,14 +5,14 @@
     $request_body = file_get_contents('php://input');
     file_put_contents('log/logs', $request_body.PHP_EOL, FILE_APPEND);
     $data = json_decode($request_body,true);
-//     $data['type']=1;
+    // $data['type']=1;
     $array = array();
+    if( $data['type'] == null ) {
+        $data['type'] = 4;
+    }
     if($data['type']){
-        if( $data['type'] == null ) {
-         $data['type'] = 0;
-        }
         $query = "SELECT `id`,`head`,`image`,`send_add`,`title` FROM `news` WHERE `send_add` > 0 AND `typestudent` = ".$data['type']." OR `typestudent` = 4 ORDER BY `news`.`send_add` DESC LIMIT 10";
-//         echo $query;
+        // echo $query;
         $reusult_test = mysqli_query($con,$query);
         while($row = mysqli_fetch_assoc($reusult_test)){
             array_push($array,$row);
