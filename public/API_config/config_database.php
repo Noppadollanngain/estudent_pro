348be116ff43bfac6dad0554f76f4bdab61cefd0
Noppadollanngain <42247295+Noppadollanngain@users.noreply.github.com>
2019-09-09 19:54:51 +0700

    Update config_database.php

--- public/API_config/config_database.php
+++ public/API_config/config_database.php
@@ -1,4 +1,4 @@
 <?php
-    // $con = mysqli_connect('us-cdbr-iron-east-03.cleardb.net','b1c3c0baf20496','0b49e8de','heroku_61c1333cb476d9b');
-    $con = mysqli_connect('127.0.0.1','root','','estudent_pro');
+    $con = mysqli_connect('us-cdbr-iron-east-03.cleardb.net','b1c3c0baf20496','0b49e8de','heroku_61c1333cb476d9b');
+    //$con = mysqli_connect('127.0.0.1','root','','estudent_pro');
     mysqli_set_charset($con,'utf8');
