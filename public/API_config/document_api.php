d6780cf529686f88db4cea7799f7f4b06c5a4e4f
Noppadollanngain <42247295+Noppadollanngain@users.noreply.github.com>
2019-09-09 21:23:33 +0700

    Update document_api.php

--- public/API_config/document_api.php
+++ public/API_config/document_api.php
@@ -8,8 +8,25 @@
     if(empty($data['id'])){
         exit(json_encode(['msg'=> 'miss']));
     }else{
-        $query = "SELECT `doc1`, `doc2`, `doc3`, `doc4`, `confrim` FROM `documents` WHERE `student` = ".$data['id'];
-        $reusult_test = mysqli_query($con,$query);
-        $result_show = mysqli_fetch_assoc($reusult_test);
-        exit(json_encode($result_show));
+        $count = "SELECT COUNT(`doc1`) as count FROM `documents` WHERE `student` = ".$data['id'];
+        $reusult_count = mysqli_query($con,$count);
+        
+        $count_fetch = mysqli_fetch_assoc($reusult_count);
+        
+        if($count_fetch['count'] > 0) {
+            $query = "SELECT `doc1`, `doc2`, `doc3`, `doc4`, `confrim` FROM `documents` WHERE `student` = ".$data['id'];
+            $reusult_test = mysqli_query($con,$query);
+            $result_show = mysqli_fetch_assoc($reusult_test);
+            exit(json_encode($result_show));
+        }else { 
+            $arr = [
+                'doc1' => null,
+                'doc2' => null,
+                'doc3' => null,
+                'doc4' => null,
+                'confrim' => null,
+            ];
+            exit(json_encode($arr));
+        }
+        
     }
